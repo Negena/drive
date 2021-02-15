cb3848a9099437b2d7cff6b114e2a37974e6e225
Negena <justnegena@gmail.com>
2021-02-15 19:52:14 +0500

    horn not finished

---
+++ move.js
@@ -0,0 +1,42 @@
+$(function(){
+  let surface = $(".surface")
+  let car = $(".car")
+  let img = $('.car img')
+  let fara = true
+  const cars = ['./assets/Img_06.png' ,'./assets/Img_05.png']
+
+//move
+  $(document).on("keypress", function(e){
+    if(e.which == 13) {
+      $(surface).toggleClass("surface_move")
+      $(car).toggleClass("car_jump")
+    }
+  })
+//light
+  $(document).on("keypress", function(e){
+    if(e.which == 119){
+      if(fara){
+        fara = false
+        img.attr('src', cars[0])
+      }
+      else {
+        fara = true
+        img.attr('src', cars[1])
+      }
+    }
+  })
+
+//sound
+let audio = document.getElementById('audio')
+let plays = true
+
+$(document).on("keydown", function(e){
+  if (e.which == 83){
+    if(plays){
+      plays = false
+        audio.play()
+    }
+  }
+})
+
+})
